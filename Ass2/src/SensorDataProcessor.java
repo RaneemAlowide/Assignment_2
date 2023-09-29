1350c4af3bd135a6036ed69fa3cb7e697500c2a4
RaneemAlowide <r.alowide@gmail.com>
2023-09-29 18:20:35 +0300

    edit code Renamed method -avrege- to -calculateAverage- & method -calculate- to -calculateData- its purpose and clarify its functionality, also rename variable -data2- to descriptive name -resultData-

--- Ass2/src/SensorDataProcessor.java
+++ Ass2/src/SensorDataProcessor.java
@@ -10,7 +10,7 @@ public class SensorDataProcessor {
         this.limit = limit;
     }
     // calculates average of sensor data
-    private double average(double[] array) {
+    private double calculateAverage(double[] array) {
         double val = 0;
         for (int i = 0; i < array.length; i++) {
             val += array[i];
@@ -20,10 +20,9 @@ public class SensorDataProcessor {
         return result;
     }
     // calculate data
-    public void calculate(double d) {
+    public void calculateData(double d) {
         int i, j, k = 0;
-        double[][][] data2 = new
-                double[data.length][data[0].length][data[0][0].length];
+        double[][][] resultData = new double[data.length][data[0].length][data[0][0].length];
         BufferedWriter out;
         // Write racing stats data into a file
         try {
@@ -31,30 +30,30 @@ public class SensorDataProcessor {
             for (i = 0; i < data.length; i++) {
                 for ( j = 0; j < data[0].length; j++) {
                     for ( k = 0; k < data[0][0].length; k++) {
-                        data2[i][j][k] = data[i][j][k] / d -
+                        resultData[i][j][k] = data[i][j][k] / d -
                                 Math.pow(limit[i][j], 2.0);
-                        if (average(data2[i][j]) > 10 && average(data2[i][j])
+                        if (calculateAverage(resultData[i][j]) > 10 && calculateAverage(resultData[i][j])
                                 < 50)
                             break;
-                        else if (Math.max(data[i][j][k], data2[i][j][k]) >
+                        else if (Math.max(data[i][j][k], resultData[i][j][k]) >
                                 data[i][j][k])
                             break;
                         else if (Math.pow(Math.abs(data[i][j][k]), 3) <
-                                Math.pow(Math.abs(data2[i][j][k]), 3)
-                                && average(data[i][j]) < data2[i][j][k] && (i + 1)
+                                Math.pow(Math.abs(resultData[i][j][k]), 3)
+                                && calculateAverage(data[i][j]) < resultData[i][j][k] && (i + 1)
                                 * (j + 1) > 0)
-                            data2[i][j][k] *= 2;
+                            resultData[i][j][k] *= 2;
                         else
                             continue;
                     }
                 }
             }
-            //a
-            for (i = 0; i < data2.length; i++) {
-    for (j = 0; j < data2[0].length; j++) {
-        for (k = 0; k < data2[0][0].length; k++) {
-            out.write(data2[i][j][k] + "\t");
-        }
+            
+            for (i = 0; i < resultData.length; i++) {
+                 for (j = 0; j < resultData[0].length; j++) {
+                    for (k = 0; k < resultData[0][0].length; k++) {
+                        out.write(resultData[i][j][k] + "\t");
+                    }
 // newline after each row
         out.newLine(); 
     }
